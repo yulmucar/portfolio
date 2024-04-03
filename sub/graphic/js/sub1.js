533aa4a18036bad3289f40564545b783eb70e735
yulmucar <fishr@naver.com>
2024-04-03 12:20:57 +0900

    240403 수정1

--- sub/graphic/js/sub1.js
+++ sub/graphic/js/sub1.js
@@ -1,4 +1,9 @@
 $(function () {
+    //해더 페이지이동
+    $('.header nav ul li a').click(function (e) {
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 600);
+    })
+    
     //스와이퍼 슬라이드
     var swiper = new Swiper(".mySwiper", {
         effect: "coverflow",
@@ -11,6 +16,8 @@ $(function () {
             depth: 100,
             modifier: 1,
             slideShadows: true,
+            loop: true,
+            centeredSlides: true,
         },
         pagination: {
             el: ".swiper-pagination",
